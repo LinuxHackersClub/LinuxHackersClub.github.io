c59e07bdd716aa258c6ecd6c9565896ab7f04a16
xezo360hye <xezo360hye@gmail.com>
2023-06-10 18:47:32 +0300

    Initial commit

---
+++ dynamic.js
@@ -0,0 +1,10 @@
+// This function will be called on page login
+function getDynemicInfo() {
+    // TODO at least the following
+    // [ ] schedule for meetings
+    // [ ] club members info
+}
+
+
+// Load dynamic resources (schedule, members etc)
+window.onload = getDynemicInfo
